88586384ebcd4cb8df9e4939cbb47682def46e9c
GuiTheDev <49080694+GuiTheDev@users.noreply.github.com>
2023-07-30 12:35:36 +0100

    a

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
   import "../app.scss";
 </script>
 
+
+
+  <a href="/"><img src="facvicon.png" alt="Redirect main page" style="width:82px;height:82px;"></a>
+  <br>
+  <br>
+  <a href="https://discord.gg/u9RQcJbx" class="som"><img src="discord-logo.png" alt="discord" style="width:48px;height:48px;"></a>
 <slot />
+
+
